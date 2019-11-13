fda814defeb57b41e78e618c2fb4c9b0840f844d
Diego Goulart <diegogoulart.aws@gmail.com>
2019-11-12 21:57:46 -0300

    configurando loaders

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.css';
+
+import childrens from './assets/childrens-1.png';
+
+function App() {
+    return <img width="200" src={childrens} />
+}
+
+export default App;

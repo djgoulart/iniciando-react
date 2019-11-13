01953a3529951fcaa8718a3e691be3c0d999df3b
Diego Goulart <diegogoulart.aws@gmail.com>
2019-11-12 23:23:25 -0300

    introdução ao react

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css';
 
-import childrens from './assets/childrens-1.png';
+import TechList from './components/TechList';
 
 function App() {
-    return <img width="200" src={childrens} />
+    return <TechList />
 }
 
 export default App;
